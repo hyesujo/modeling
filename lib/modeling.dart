a89c577208b234b1df2ca8984dfcbc3dbe5082d4
조혜수 <poer611@naver.com>
2020-07-25 21:46:43 +0900

    모델링

--- lib/modeling.dart
+++ lib/modeling.dart
@@ -1,117 +1,118 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(MyApp());
+  
+  
+
+  List<Map<String, dynamic>> data = [
+    { "title": " 댄스", "src" : "","data" : [
+      {"title" : "댄스노래1","name" : "가수1", "src" : "", "des" : ""},
+      {"title" : "댄스노래2", "name" : "가수2","src" : "", "des" : ""},
+      { "title" : "댄스노래3", "name" : "가수3","src" : "", "des" : ""}
+      ]
+    },
+   
+    {"title": " 발라드","src" : "","data" : [
+        { "title" : "발라드노래1", "name" : "가수1", "src" : "", "des" : "" },
+        { "title" : "발라드노래2","name" : "가수2","src" : "", "des" : ""},
+        { "title" : "발라드노래3", "name" : "가수3",  "src" : "",  "des" : "" }
+      ]
+    },
+  ];
+  
+  
+  List<Models> result;
+  
+  result = data.map((e) =>Models.toItem(item: e)).toList();
+  
+  print(result[1].title);
+  print(result[0].data[1].name);
+  print(result[1].data[0].title);
+
+  
+//   List<Models> result = data.map<Models>((Map<String,dynamic>e){
+//     return new Models(
+//     title: e["title"],
+//     src : e["src"],
+//      data: e["data"].map<DeepModels>((c) {
+//        return new DeepModels(
+//        title: c["title"],
+//        name: c["name"],
+//        des : c["des"],
+//         src : c["src"]   
+//        );
+//      }).toList()
+//     );
+//   }).toList();
+  
+//   print(result);
+//   print(result[1].title);
+//   print(result[0].data[2].title);
+  
+  
+//   List<int> data = [1,2,3];
+  
+//   for(int i=0; i<data.length; i++){
+//     print(data[i]);
+//   }
+
+//            //인스턴스     
+//   List<int> newData = data.map((int e) {
+//       print(e);
+//    return e;
+//   }).toList();
+  
+//   print(newData);
+
+//            //= [];를 안붙이면 선언만 한상태 
+//            //(리스트의 인트형을 담은 것이 fordata라고 선언만 한상태)
+// //  List<int> forData =[];
+// // for(int i =0; i<data.length; i++) {
+// //   forData.add(data[i]++);
+// // }
+// //   print(forData);
+
+  
+//  data.forEach((int e) {
+//    print(e);
+//  });
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+class Models {
+  String title;
+  String src;
+  List<DeepModels> data;
+  
+ Models({ this.data,this.src,this.title}) 
+   :assert(data != null), assert(title !=null), assert(src !=null);
+  
+  factory Models.toItem(
+    { Map<String, dynamic> item}) {
+    return new Models(
+    title: item["title"],
+    src: item["src"],
+    data : item["data"].map<DeepModels>((e)=> DeepModels.toItem(e : e )).toList());
+    
+  }
+}
+
+//   {"title" : "댄스노래1","name" : "가수1", "src" : "", "des" : ""},
+class DeepModels{
+  String title;
+  String name;
+  String src;
+  String des;
+  
+  DeepModels({
+    this.des,this.name,this.src,this.title}) 
+    :assert(title != null);
+  
+  
+  factory DeepModels.toItem({Map<String, dynamic> e}) {
+    return new DeepModels(
+      title: e["title"],
+      des : e["des"],
+      src : e["src"],
+      name : e["name"]
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  
 }
